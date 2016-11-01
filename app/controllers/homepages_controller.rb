8446626707974e8b3f440f8c73b6c5db01168fc2
Briana Eng <brianaeng@gmail.com>
2016-11-01 09:05:15 -0700

    Class stuff

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -5,14 +5,27 @@
 
 class HomepagesController < ApplicationController
   def index
-
+    @data = Slack_Api_Wrapper.list_channels
   end
 
   def create
+    # raise
+    # Use raise to see params needed for a message in rails server console
 
+    # response = Slack_Api_Wrapper.send_message(params[:channel], params[:message])
+    response = Slack_Api_Wrapper.sendmsg(params[:channel], params[:message])
+
+    if response["ok"]
+      flash[:notice] = "Post successfully sent to #{params[:channel_name]}."
+    else
+      flash[:notice] = "Post failed to send to #{params[:channel_name]}."
+    end
+
+    redirect_to root_path
   end
 
   def new
-
+    @channel = params[:name]
+    @channel_id = params[:id]
   end
 end
--- lib/slack_api_wrapper.rb
+++ lib/slack_api_wrapper.rb
@@ -1,7 +1,45 @@
 require 'httparty'
 
 class Slack_Api_Wrapper
+  TOKEN = ENV["SLACK_TOKEN"]
+  BASE_URL = "https://slack.com/api/"
 
+  def self.list_channels
+    url = BASE_URL + "channels.list?token=#{TOKEN}"
 
+    response = HTTParty.get(url)
+
+    my_channels = []
+
+    response["channels"].each do |channel_hash|
+      id = channel_hash["id"]
+      name = channel_hash["name"]
+      my_channels.push(Slack_Channel.new(name, id))
+    end
+
+    my_channels
+  end
+
+  # def self.send_message(channel, msg)
+  #   url = BASE_URL + "chat.postMessage?token=#{TOKEN}" + "&channel=#{channel}" + "&text=#{msg}"
+  #
+  #   response = HTTParty.get(url)
+  # end
+
+  def self.sendmsg(channel, msg, token = nil)
+    token = TOKEN if token == nil
+    puts "Sending message to channel #{channel}: #{msg}"
+
+    url = BASE_URL + "chat.postMessage?" + "token=#{token}"
+    data = HTTParty.post(url,
+    body:  {
+      "text" => "#{msg}",
+      "channel" => "#{channel}",
+      "username" => "cat",
+      "icon_emoji" => ":heart_eyes_cat:",
+      "as_user" => "false"
+    },
+    :headers => { 'Content-Type' => 'application/x-www-form-urlencoded' })
+  end
 
 end
